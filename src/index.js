e4e2af81dd4d460257cc68ec619d38378ef48f7e
Robson Dourado <webdesigner@allims.com.br>
2019-07-05 15:18:56 -0300

    Calculadora em ReactJS - Commit Final

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Calculator from './main/Calculator'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Fragment>
+    <h1>Calculadora</h1>
+    <Calculator />
+  </Fragment>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
